0d9739de08c34f41af4963c57c174dd31bcafddc
unknown <Leonardo Solano>
2025-04-28 22:42:46 -0600

    Se ha creado el metodo constructor del array y se ha intentado crear los metodos

--- Laboratorio 2/JuegoBatalla.java
+++ Laboratorio 2/JuegoBatalla.java
@@ -1,12 +1,19 @@
 
 import java.util.Scanner;
+import java.util.Random;
 
 public class JuegoBatalla {
 
     public static void main (String [] args){
 
-        //Atributos
-        private Robot [] robots;
+    }
+
+    //Atributos
+    private Robot[] robots;
+
+    public JuegoBatalla (int cantidadDeRobots){
+
+        robots = new Robot (cantidadDeRobots);
     }
 
     //Metodos
@@ -17,8 +24,21 @@ public class JuegoBatalla {
     }
 
     public Robot [] creacionRobots (){
-        
+
+        Scanner = new Scanner (System.in);
+        System.out.println ("Ingrese la cantidad de robots que desea enfrentar");
+
+        int cantidadDeRobots = nextInt();
+        nextInt ();
+
+        Robot [] nuevosRobots = new Robot [cantidadDeRobots];
+
+        for (int i = 0; i < robots.length; i++);
+
+        System.out.println("Ingrese el nombre del robot" + (i+1));
+        String nombre = nextInt ();
     }
+
     public void iniciarBatalla (){
 
     }
