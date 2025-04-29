ee1ec343598598e8622b530789909ce8cc0b2968
unknown <Leonardo Solano>
2025-04-28 18:20:20 -0600

    Se agregaron los setter y los atributos

--- Laboratorio 2/Robots.java
+++ Laboratorio 2/Robots.java
@@ -3,4 +3,25 @@ public class Robots {
     private String nombre;
     private Double puntosDeVida;
     private Double ataque;
+
+    public Robots (String nombre, Double puntosDeVida, Double ataque) {
+        
+        this.nombre = nombre;
+        this.puntosDeVida = puntosDeVida;
+        this.ataque = ataque;
+    }
+
+    //Setters
+
+    public void SetNombre (String nombre){
+        this.nombre = nombre;
+    }
+
+    public void SetPuntosDeVida (Double puntosDeVida){
+        this.puntosDeVida = puntosDeVida;
+    }
+
+    public void SetAtaque (Double ataque){
+        this.ataque = ataque;
+    }
 }
