8ae6310aea4116f7db216708e4bdc023a1934e0b
Alondra387 <alondramelissaap@gmail.com>
2025-05-06 18:38:48 -0600

    Creo los atributos de la clase ahorcado y el método constructor

--- Proyecto1/Ahorcado.java
+++ Proyecto1/Ahorcado.java
@@ -1,3 +1,19 @@
 public class Ahorcado{
+    private String palabraSecreta;
+    private char[] progreso;
+    private char[] letrasAdivinadas;
+    private int intentosRestantes;
+    private int cantidadLetrasAdivinadas;
+
+
+public Ahorcado (String palabra){
+    this.palabraSecreta = palabra.toLowerCase();
+    this.intentosRestantes = 6;
+    this.progreso = new char [palabra.length()];
+
+    for (int i = 0; i < palabra.length(); i++){
+    progreso[i] = '_';
+    }
+}
 
 }
