62d96cba3246043059b9590b962500aeaaa6c450
Alondra387 <alondramelissaap@gmail.com>
2025-05-07 09:23:35 -0600

    Creo en la clase Ahorcado el método adivinarLetra y le pongo comentarios al código para mayor legibilidad

--- Proyecto1/Ahorcado.java
+++ Proyecto1/Ahorcado.java
@@ -45,6 +45,48 @@ public boolean haPerdido() {
 
 
 
+//métodos de la clase
+
+//método que recibe la letra y le dice al usuario si adivinó correctamente y muestra el progreso de la partida
+public void adivinarLetra(char letra){
+    letra = Character.toLowerCase(letra);
+    for (int i = 0; i < cantidadLetrasAdivinadas; i++){
+        if (letrasAdivinadas[i] == letra){
+            System.out.println("Ya intentó con esta letra");
+            return;
+        }
+    }
+
+    letrasAdivinadas[cantidadLetrasAdivinadas] = letra;
+    cantidadLetrasAdivinadas++;
+
+    
+    boolean acierto = false;
+    for (int i = 0; i < palabraSecreta.length(); i++){
+        if (palabraSecreta.charAt(i) == letra ){
+            progreso[i] = letra;
+            acierto = true;
+        }
+
+        if (!acierto){
+            intentosRestantes--;
+            System.out.println("La letra que eligió no se encuentra en la palabra secreta.Le quedan la siguiente cantidad de intentos: " + intentosRestantes);
+        }else{
+            System.out.println ("Adivinó una nueva letra correctamente");
+        }
+    
+    
+        System.out.println("Progreso de la actual partida:");
+        for (int j = 0; j < progreso.length; j++){
+            System.out.print(progreso[j] + " ");
+        }
+        System.out.println();
+    }
+
+
+    
+}
+
 
 
 
