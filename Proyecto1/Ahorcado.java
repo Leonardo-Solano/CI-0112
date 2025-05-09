e5b3ab58a0bb8e0ba239cf744fff3e77b80bcf58
Alondra387 <alondramelissaap@gmail.com>
2025-05-08 20:27:28 -0600

    Creo en la clase Ahorcado los métodos haGanado y haPerdido

--- Proyecto1/Ahorcado.java
+++ Proyecto1/Ahorcado.java
@@ -9,73 +9,90 @@ public class Ahorcado{
 
 
 //constructor de la clase
-public Ahorcado (String palabra){
-    this.palabraSecreta = palabra.toLowerCase();
-    this.intentosRestantes = 6;
-    this.progreso = new char [palabra.length()];
+    public Ahorcado (String palabra){
+        this.palabraSecreta = palabra.toLowerCase();
+        this.intentosRestantes = 6;
+        this.progreso = new char [palabra.length()];
 
-    for (int i = 0; i < palabra.length(); i++){
-    progreso[i] = '_';
+        for (int i = 0; i < palabra.length(); i++){
+        progreso[i] = '_';
+        }
+        this.letrasAdivinadas = new char[palabra.length() + 10];
     }
-    this.letrasAdivinadas = new char[palabra.length() + 10];
-}
 
 
 
 //getters de la clase
-public int getIntentosRestantes() {
-    return intentosRestantes;
-}
+    public int getIntentosRestantes() {
+        return intentosRestantes;
+    }
 
-public String getProgreso() {
-    return new String(progreso);
-}
+    public String getProgreso() {
+        return new String(progreso);
+    }
 
 
 
 //métodos de la clase
 
 //método que verifica los intentos de adivinar letras
-//Este fragmento sirve para saber si la letra que se está intentando ya se adivinó
-public void adivinarLetra(char letra){
-    letra = Character.toLowerCase(letra);
-    for (int i = 0; i < cantidadLetrasAdivinadas; i++){
-        if (letrasAdivinadas[i] == letra){
-            System.out.println("Ya intentó con esta letra");
-            return;
+    //Este fragmento sirve para saber si la letra que se está intentando ya se adivinó
+    public void adivinarLetra(char letra){
+        letra = Character.toLowerCase(letra);
+        for (int i = 0; i < cantidadLetrasAdivinadas; i++){
+            if (letrasAdivinadas[i] == letra){
+                System.out.println("Ya intentó con esta letra");
+                return;
+            }
         }
-    }
-    letrasAdivinadas[cantidadLetrasAdivinadas] = letra;
-    cantidadLetrasAdivinadas++;
+        letrasAdivinadas[cantidadLetrasAdivinadas] = letra;
+        cantidadLetrasAdivinadas++;
 
 
-//Esto verifica si la letra ingresada está en la palabra secreta
-    boolean acierto = false;
-    for (int i = 0; i < palabraSecreta.length(); i++){
-        if (palabraSecreta.charAt(i) == letra ){
-            progreso[i] = letra;
-            acierto = true;
+    //Esto verifica si la letra ingresada está en la palabra secreta
+        boolean acierto = false;
+        for (int i = 0; i < palabraSecreta.length(); i++){
+            if (palabraSecreta.charAt(i) == letra ){
+                progreso[i] = letra;
+                acierto = true;
+            }
         }
+
+
+    //Este fragmento es para saber si la letra fue un acierto
+        if (!acierto){
+            intentosRestantes--;
+            System.out.println("La letra que eligió no se encuentra en la palabra secreta.Le quedan la siguiente cantidad de intentos: " + intentosRestantes);
+        }else{
+            System.out.println ("Adivinó una nueva letra correctamente");
+        }
+
+
+    //Muestra el progreso de la partida
+        System.out.println("Progreso de la actual partida:");
+        for (int j = 0; j < progreso.length; j++){
+            System.out.print(progreso[j] + " ");
+        }
+        System.out.println();
+
+    }
+
+    
+//Método que verifica si se ganó la partida
+    public boolean haGanado() {
+        for (int i = 0; i < progreso.length; i++ ){
+            if (progreso[i] == '_'){
+            return false;
+            }
+        }
+    return true;
     }
 
 
-//Este fragmento es para saber si la letra fue un acierto
-    if (!acierto){
-        intentosRestantes--;
-        System.out.println("La letra que eligió no se encuentra en la palabra secreta.Le quedan la siguiente cantidad de intentos: " + intentosRestantes);
-    }else{
-        System.out.println ("Adivinó una nueva letra correctamente");
+//Método que verifica si ya perdió todos los intentos y por lo tanto perdió
+    public boolean haPerdido(){
+        return intentosRestantes <= 0;
     }
-
-
-//Muestra el progreso de la partida
-    System.out.println("Progreso de la actual partida:");
-    for (int j = 0; j < progreso.length; j++){
-        System.out.print(progreso[j] + " ");
-    }
-    System.out.println();
-
-}
 }
 
 
