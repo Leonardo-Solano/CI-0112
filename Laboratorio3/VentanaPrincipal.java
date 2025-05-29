b62aeabfc35daf4c1c9699dc1f9ed748b3363a98
Leonardo-Solano <leonardosolanoramirez6@gmail.com>
2025-05-29 12:05:01 -0600

    Se crea una ventana llamada archivo que despliega las opciones de nuevo, guardar y salir

--- Laboratorio3/VentanaPrincipal.java
+++ Laboratorio3/VentanaPrincipal.java
@@ -8,6 +8,21 @@ public class VentanaPrincipal extends JFrame{
         setSize (400,300);
         setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo (null); //Esto cierra la ventana
+
+        JMenuBar menuBar = new JMenuBar ();
+        JMenu menuArchivo = new JMenu ("Archivo");
+        JMenuItem nuevoItem = new JMenuItem ("Nuevo");
+        JMenuItem guardarItem = new JMenuItem ("Guardar");
+        JMenuItem salirItem = new JMenuItem ("Salir");
+
+
+        menuArchivo.add (nuevoItem);
+        menuArchivo.add (guardarItem);
+        menuArchivo.addSeparator (); //Ayuda a hacer una sepracion visual
+        menuArchivo.add (salirItem);
+        menuBar.add (menuArchivo);
+        setJMenuBar (menuBar);
+
     }
 
     public static void main (String [] args){
